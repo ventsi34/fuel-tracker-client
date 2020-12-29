b31f8268069211a25c1c4af96001fe957ddaf9b1
Ventsislav Dimitrov <Ventsislav.Dimitrov@ventsislav.dimitrov-mbp>
2020-12-29 10:58:21 +0200

    added guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
-  { path: 'vehicles/:vehicleId/trips', loadChildren: () => import('./trip/trip.module').then(m => m.TripModule) },
-  { path: 'vehicles', loadChildren: () => import('./vehicle/vehicle.module').then(m => m.VehicleModule) },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+  {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'vehicles/:vehicleId/trips',
+    loadChildren: () => import('./trip/trip.module').then(m => m.TripModule)
+  },
+  {
+    path: 'vehicles',
+    loadChildren: () => import('./vehicle/vehicle.module').then(m => m.VehicleModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  }
 ];
 
 @NgModule({
